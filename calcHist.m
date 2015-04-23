2c7733e3cb209d510f81fbbe1fa0c7e4bfbe8727
linme882@student.liu.se <linme882@student.liu.se>
2015-04-23 15:59:27 +0200

    bitshift now works and we have a histogram of 512 bins.

--- calcHist.m
+++ calcHist.m
@@ -1,26 +1,22 @@
 function [ out ] = calcHist( im ) 
-%First reshape to one column
-%reshapedim=reshape(im(:,:,2),[size(im,1)*size(im,2) 1]);
-%imhistogram=hist(reshapedim,[0:1:255])';
-%imhistogram=imhistogram./sum(imhistogram); %Normalization
-%out = imhistogram;
+% set to 0 and 255 to always get 512 bins, quick fix for now. 
+im = uint16(im); 
 r = im(:,:,1);
+r(1)=0;r(2)=255;
 g = im(:,:,2);
+g(1)=0;g(2)=255;
 b = im(:,:,3);
+b(1)=0;b(2)=255;
 
 % 8 bins, 8bits. shift 5. 1 for indexing, matlab start with 1. 
 rg=bitshift(bitshift(g,-5),3) + bitshift(b,-5) + 1;
-rgb = bitshift(bitshift(r,-5),6) + bitshift(bitshift(g,-5),3) + bitshift(b,-5) + 1
-
-max(max(rgb))
-
+rgb = bitshift(bitshift(r,-5),6) + bitshift(bitshift(g,-5),3) + bitshift(b,-5) + 1;
 
 % to get the histogram right.. http://stackoverflow.com/questions/24097286/rgb-histogram-using-bitshift-in-matlab
 
 %// Make sure these are column vectors
 index = rgb(:);
 weights = ones(numel(index), 1);
-numel(index)
 
 %// Calculate histogram
 h = accumarray(index, weights);
